0f592517ae8c8794470f2e20400509db706fb531
Tofiq Quadri <tofiqquadri@ymail.com>
2023-03-06 20:02:07 +0530

    # [Tofiq] Rapidata AI Front-End Architecture Setup

---
+++ src/Containers/Layout/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div>
+            <span>Footer</span>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/Containers/Layout/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <span>Header</span>
+        </div>
+    );
+};
+
+export default Header;
